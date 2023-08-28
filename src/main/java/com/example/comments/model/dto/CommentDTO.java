f8e2b0b312c9053a84ce4f04df64cfc4ab6c371e
Sitthata <aprils96950@gmail.com>
2023-08-28 13:01:25 +0700

    Convert Dto into records

--- src/main/java/com/example/comments/model/dto/CommentDTO.java
+++ src/main/java/com/example/comments/model/dto/CommentDTO.java
@@ -4,59 +4,6 @@ import com.example.comments.model.User;
 
 import java.util.List;
 
-public class CommentDTO {
-    private Long id;
-    private String content;
-    private String createdAt;  // Notice it's a String, so you can use your TimeAgoUtility directly
-    private Integer score;
-    private User user;
-    private List<CommentDTO> replies;
+public record CommentDTO(Long id, String content, String TimeAgo, Integer score, User user, List<CommentDTO> replies) {
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public List<CommentDTO> getReplies() {
-        return replies;
-    }
-
-    public void setReplies(List<CommentDTO> replies) {
-        this.replies = replies;
-    }
 }
--- src/main/java/com/example/comments/model/dto/DtoConverter.java
+++ src/main/java/com/example/comments/model/dto/DtoConverter.java
@@ -3,23 +3,24 @@ package com.example.comments.model.dto;
 import com.example.comments.Utils.TimeAgoUtility;
 import com.example.comments.model.Comment;
 
+import java.sql.Time;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DtoConverter {
     public static CommentDTO toDto(Comment comment) {
-        CommentDTO commentDTO = new CommentDTO();
-        commentDTO.setId(comment.getId());
-        commentDTO.setContent(comment.getContent());
-        commentDTO.setCreatedAt(TimeAgoUtility.getTimeAgo(comment.getCreatedAt()));
-        commentDTO.setScore(comment.getScore());
-        commentDTO.setUser(comment.getUser());
-
-        // Recursive call to set the replies
+        List<CommentDTO> repliesDto = null;
         if (comment.getReplies() != null) {
-            commentDTO.setReplies(comment.getReplies().stream().map(DtoConverter::toDto).collect(Collectors.toList()));
+            repliesDto = comment.getReplies().stream().map(DtoConverter::toDto).collect(Collectors.toList());
         }
-        return commentDTO;
+        return new CommentDTO(
+                comment.getId(),
+                comment.getContent(),
+                TimeAgoUtility.getTimeAgo(comment.getCreatedAt()),
+                comment.getScore(),
+                comment.getUser(),
+                repliesDto
+        );
     }
 
     public static List<CommentDTO> toDtoList(List<Comment> comments) {
